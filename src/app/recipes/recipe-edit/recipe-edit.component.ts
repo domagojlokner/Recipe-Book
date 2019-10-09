48a51320044a0c407d93dfab7ca2e4265b371250
Domagoj Lokner <dlokner@egcp.com>
2019-10-09 09:09:48 +0200

    unused import

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,3 @@
-import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
